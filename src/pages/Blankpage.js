782685bd5a27817782601db99429901b74ec3802
DineshMathi17 <dineshbabum17d@gmail.com>
2023-06-20 14:40:02 +0530

    first commit

---
+++ src/pages/Blankpage.js
@@ -0,0 +1,11 @@
+import Sidebar from "../components/Sidebar";
+
+export function BlankPage(){
+    return(
+        <Sidebar>
+        <div className='main_margin'>
+            <h1 style={{textAlign:'initial'}}>Blank Page</h1>
+        </div>
+        </Sidebar>
+    )
+}
